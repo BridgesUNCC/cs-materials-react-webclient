0382a474e44688547906a9ca8d1cf8f9ca04eb7a
quaiquai <mmcquaig@uncc.edu>
2020-10-14 18:27:24 -0400

    typos

--- src/components/tutorials/AnalyzeTutorial.tsx
+++ src/components/tutorials/AnalyzeTutorial.tsx
@@ -63,7 +63,7 @@ export const AnalyzeTutorial: FunctionComponent = ({ }) => {
         </Typography>
         <img src={require('../../common/images/analyzetut_1.PNG')} alt="Italian Trulli"></img>
         <Typography variant="body1" className={classes.body} gutterBottom>
-          First, you will need to first click the 'BEGIN' button on the center section of the
+          First, you will need to click the 'BEGIN' button on the center section of the
           home page titled 'Analyze'. This section of the website is where all analysis
           of the materials and collections are performed. Clicking begin will
           bring you to a blank page with a navigation bar on the left side of the screen labeled
@@ -74,19 +74,19 @@ export const AnalyzeTutorial: FunctionComponent = ({ }) => {
         </Typography>
         <Typography variant="body1" className={classes.body} gutterBottom>
           There are three different ways of selecting materials and collections before
-          the analyzing process. Each selection type will provide different visualizations
-          when navigating to visual pages. In the 'Select Materials' tab,
+          the analysis process. Each selection type will provide different visualizations
+          when navigating to visual pages.
         </Typography>
         <Typography variant="h4" className={classes.subHeading}>
           Select Materials
         </Typography>
         <img src={require('../../common/images/analyzetut_2.PNG')} alt="Italian Trulli"></img>
         <Typography variant="body1" className={classes.body} gutterBottom>
-          You will see a page listing all materials within the CS Materials database.
+           In the 'Select Materials' tab, you will see a page listing all materials within the CS Materials database.
         </Typography>
         <img src={require('../../common/images/analyzetut_3.PNG')} alt="Italian Trulli" ></img>
         <Typography variant="body1" className={classes.body} gutterBottom>
-          Each material has an associated check box that you can click to select a material.
+          Each material has an associated check box that you can click to select the material.
           Multiple materials can be selected at one time. The list also provides a
           'select all' and 'select none' buttons to select every material in the list or remove
           all selections respectively.
@@ -115,7 +115,7 @@ export const AnalyzeTutorial: FunctionComponent = ({ }) => {
         <Typography variant="body1" className={classes.body} gutterBottom>
           Within this layout, leaf nodes represent tags while inner nodes are the higher level ACM classifications. The colors blue,
           orange, gray, and red colors represent the parent nodes of tags, tags that have been hit/classified by a material,
-          non hit tags, and the root node respectively. Hovering over a tag will display all the materials assciated with it in the top right
+          non hit tags, and the root node respectively. Hovering over a tag will display all the materials associated with it in the top right
           and the path to the node from the root on the left.
         </Typography>
         <Typography variant="h4" className={classes.subHeading}>
@@ -128,7 +128,7 @@ export const AnalyzeTutorial: FunctionComponent = ({ }) => {
         <Typography variant="body1" className={classes.body} gutterBottom>
           Within this layout, leaf nodes represent tags while inner nodes are the higher level PDC classifications. The colors blue,
           orange, gray, and red colors represent the parent nodes of tags, tags that have been hit/classified by a material,
-          non hit tags, and the root node respectively. Hovering over a tag will display all the materials assciated with it in the top right
+          non hit tags, and the root node respectively. Hovering over a tag will display all the materials associated with it in the top right
           and the path to the node from the root on the left.
         </Typography>
         <Typography variant="h4" className={classes.subHeading}>
@@ -141,17 +141,17 @@ export const AnalyzeTutorial: FunctionComponent = ({ }) => {
         <Typography variant="body1" className={classes.body} gutterBottom>
           This matrix view shows the materials listed on the x axis and the union of all tags on the y axis.
           The matrix is biclustered by similar tags for easy identification of patterns for similar materials.
-          This view allows the user to easily make classification edits to their material by click in cells
+          This view allows the user to easily make classification edits to their material by clicking in cells
           corresponding to the material and tag. Clicking a cell will turn it blue (assigned tag to that material),
           gray (removed tag from that material), or red (reverting the removal of a tag).
         </Typography>
         <img src={require('../../common/images/analyzetut_8.PNG')} alt="Italian Trulli"></img>
         <Typography variant="body1" className={classes.body} gutterBottom>
-          Hovering over a cell will highlight it and display the 'breadcrumbs' (path in ACM tree) to that tags
+          Hovering over a cell will highlight it and display the 'breadcrumbs' (path in ACM tree) to that tag
           along with the materials associated with it.
         </Typography>
         <Typography variant="body1" className={classes.body} gutterBottom>
-          All of these Views can also be performed on collections of materials.
+          All of these views can also be performed on collections of materials.
           By visiting the 'Select Collections' tab, the user can select collections from a similar list
           and proceed by clicking the different vizualization tabs.
         </Typography>
@@ -169,12 +169,6 @@ export const AnalyzeTutorial: FunctionComponent = ({ }) => {
           information from the lists will be combined for the visualizations.
         </Typography>
         <img src={require('../../common/images/analyzetut_10.PNG')} alt="Italian Trulli"></img>
-        <Typography variant="body1" className={classes.body} gutterBottom>
-          From here, the user can begin to select multiple materials from the list. At the top of each list
-          is three tabs. These tabs manipulate each list to display either materials, collections, or your materials if
-          you have created some. You can make selections from any of the lists without affecting the other. All selected
-          information from the lists will be combined for the visualizations.
-        </Typography>
         <Typography variant="body1" className={classes.body}>
           Once the materials/collections desired are selected, 2 different visualization types can be performed
           on them:
@@ -194,10 +188,10 @@ export const AnalyzeTutorial: FunctionComponent = ({ }) => {
         <img src={require('../../common/images/analyzetut_11.PNG')} alt="Italian Trulli"></img>
         <Typography variant="body1" className={classes.body} gutterBottom>
           Within this layout, leaf nodes represent tags while inner nodes are the higher level ACM classifications. A divergent color scale
-          is used to represent the number of materials assocciated with a given tag from each list. The darker the color purple or orange, the more that
+          is used to represent the number of materials associated with a given tag from each list. The darker the color purple or orange, the more that
           tag is covered from the first or second list respectively. The whiter the node, the more both lists cover that tag evenly.
           Hovering over a node will display all the materials
-          assciated with it in the top right with the percentage of coverage from each list
+          associated with it in the top right with the percentage of coverage from each list
           and the path to the node from the root on the left.
         </Typography>
         <Typography variant="h4" className={classes.subHeading}>
@@ -209,10 +203,10 @@ export const AnalyzeTutorial: FunctionComponent = ({ }) => {
         <img src={require('../../common/images/analyzetut_12.PNG')} alt="Italian Trulli"></img>
         <Typography variant="body1" className={classes.body} gutterBottom>
           Within this layout, leaf nodes represent tags while inner nodes are the higher level PDC classifications. A divergent color scale
-          is used to represent the number of materials assocciated with a given tag from each list. The darker the color purple or orange, the more that
+          is used to represent the number of materials associated with a given tag from each list. The darker the color purple or orange, the more that
           tag is covered from the first or second list respectively. The whiter the node, the more both lists cover that tag evenly.
           Hovering over a node will display all the materials
-          assciated with it in the top right with the percentage of coverage from each list
+          associated with it in the top right with the percentage of coverage from each list
           and the path to the node from the root on the left.
         </Typography>
 
